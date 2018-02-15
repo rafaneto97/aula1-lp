53e7cfa73b04a0b938a8c10e681053ce99f67a0a
rafaneto97 <rafaneto97@users.noreply.github.com>
2018-02-15 16:14:37 +0000

    Ola Mundo

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -10,8 +10,10 @@ namespace OlaMundo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ola esta frase é uma merda");
-            Console.ReadKey();
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
         }
     }
 }
